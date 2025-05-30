bc8acf01e1d4337cb41a4e4a876c5c01d691f2ed
鈴木 色音 <k024g0058@g.neec.ac.jp>
2025-05-30 12:05:06 +0900

    MT3_03_01

--- main.cpp
+++ main.cpp
@@ -38,15 +38,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//--------- 変数 ---------//
 
     Camera camera(
-		{ 0.0f, 0.0f, 8.0f },
+		{ 0.0f, 0.0f, -8.0f },
 		{ 0.3f, 0.0f, 0.0f },
 		{ 1.0f, 1.0f, 1.0f }
 	);
 
-    Vector3 controlPoint[3] = {
-		{ -0.8f, 0.58f, 1.0f  },
-        { 1.78f, 1.0f,  -0.3f },
-        { 0.94f, -0.7f, 2.3f  }
+	Vector3 translates[3] = {
+		{ 0.2f, 1.0f, 0.0f },
+		{ 0.4f, 0.0f, 0.0f },
+		{ 0.3f, 0.0f, 0.0f }
+    };
+
+	Vector3 rotates[3] = {
+        { 0.0f, 0.0f, -6.8f },
+        { 0.0f, 0.0f, -1.4f },
+        { 0.0f, 0.0f, 0.0f }
+    };
+
+	Vector3 scales[3] = {
+		{ 1.0f, 1.0f, 1.0f },
+        { 1.0f, 1.0f, 1.0f },
+        { 1.0f, 1.0f, 1.0f }
     };
     
 	// ウィンドウの×ボタンが押されるまでループ
@@ -63,9 +75,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		
         ImGui::Begin("window");
-        ImGui::DragFloat3("controlPoint[0]", &controlPoint[0].x, 0.01f);
-        ImGui::DragFloat3("controlPoint[1]", &controlPoint[1].x, 0.01f);
-        ImGui::DragFloat3("controlPoint[2]", &controlPoint[2].x, 0.01f);
+        ImGui::DragFloat3("translates[0]", &translates[0].x, 0.01f);
+        ImGui::DragFloat3("rotates[0]", &rotates[0].x, 0.01f);
+        ImGui::DragFloat3("scales[0]", &scales[0].x, 0.01f);
+        ImGui::DragFloat3("translates[1]", &translates[1].x, 0.01f);
+        ImGui::DragFloat3("rotates[1]", &rotates[1].x, 0.01f);
+        ImGui::DragFloat3("scales[1]", &scales[1].x, 0.01f);
+        ImGui::DragFloat3("translates[2]", &translates[2].x, 0.01f);
+        ImGui::DragFloat3("rotates[2]", &rotates[2].x, 0.01f);
+        ImGui::DragFloat3("scales[2]", &scales[2].x, 0.01f);
         ImGui::End();
 
         // カメラの移動
@@ -75,6 +93,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
         // 各種行列の計算
         camera.CalculateMatrix();
+
+		// 描画用の行列の計算
+		AffineMatrix localMatrix[3];
+		localMatrix[0].SetSRT(scales[0], rotates[0], translates[0]);
+        localMatrix[1].SetSRT(scales[1], rotates[1], translates[1]);
+        localMatrix[2].SetSRT(scales[2], rotates[2], translates[2]);
+        Matrix4x4 worldMatrix[3];
+		worldMatrix[0] = localMatrix[0].GetWorldMatrix();
+        worldMatrix[1] = localMatrix[1].GetWorldMatrix() * worldMatrix[0];
+        worldMatrix[2] = localMatrix[2].GetWorldMatrix() * worldMatrix[1];
+
+		Sphere sphere[3];
+        sphere[0] = Sphere(Vector3().Transform(worldMatrix[0]), 0.05f);
+        sphere[1] = Sphere(Vector3().Transform(worldMatrix[1]), 0.05f);
+        sphere[2] = Sphere(Vector3().Transform(worldMatrix[2]), 0.05f);
+
+		Segment segment[2] = {
+            { sphere[0].center, sphere[1].center - sphere[0].center },
+            { sphere[1].center, sphere[2].center - sphere[1].center }
+        };
 
 		///
 		/// ↑更新処理ここまで
@@ -87,15 +125,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         VectorScreenPrintf(0, 0, camera.GetTranslate(), "Camera Position");
         VectorScreenPrintf(0, 32, camera.GetRotate(), "Camera Rotation");
         DrawGrid(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 2.0f, 16);
-        DrawBezier(
-            controlPoint[0],
-            controlPoint[1],
-            controlPoint[2],
-            camera.GetWVPMatrix(),
-            camera.GetViewportMatrix(),
-            0x0000FFFF,
-            64
-        );
+        segment[0].Draw(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 0xFFFFFFFF);
+        segment[1].Draw(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 0xFFFFFFFF);
+        sphere[0].Draw(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 16, 0xFF0000FF);
+        sphere[1].Draw(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 16, 0x00FF00FF);
+        sphere[2].Draw(camera.GetWVPMatrix(), camera.GetViewportMatrix(), 16, 0x0000FFFF);
 
 		///
 		/// ↑描画処理ここまで
